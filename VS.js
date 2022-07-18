f31e9ad1695d78f75654c76f2db178c4e84f856b
SerDmi <nekliudov2001@mail.ru>
2022-07-18 20:38:43 +0300

    NewIsmToday

--- VS.js
+++ VS.js
@@ -9,3 +9,47 @@ let Obj = [
 
 const sumzp = Obj.reduce((total, num) => total + Obj.zp, 0)
 console.log (typeof(sumzp))
+class NewClass {
+    constructor(people){
+        this.name = people.name
+        this.age = people.age
+        this.zp =  people.zp
+    }
+       voice(ime){
+        console.log('Hello' + ' ' + ime)
+       }
+       job(){
+        console.log('I work for' + ' ' + `${this.zp}`)
+       }
+    }
+    
+    
+    class People2 extends NewClass{
+        constructor(people){
+            super(people)
+            this.car = people.car
+        }
+        voice (ime){
+            super.voice(5000)
+            console.log('I am god' + ' ' + ime)
+        
+        }
+        get newzp(){
+          return this.zp * 3
+        }
+        set newzp(zp2){
+            this.zp = zp2
+        }
+    }
+    
+    
+    let Victor = new People2({
+    name: 'Victor',
+    age: 30,
+    zp: 35000,
+    car: 'BMW 8'
+    })
+    Victor.newzp = 30000
+    console.log(Victor)
+    Victor.voice(500000)
+    Victor.job()
